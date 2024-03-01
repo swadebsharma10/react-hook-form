5daa394434ceae478fa2bd5f98151ec7c0ffa817
swadebsharma6 <swadebriva2012@gmail.com>
2024-03-01 22:50:03 +0600

    explore more abourt react hook form

--- src/components/forms/RegistrationForm.jsx
+++ src/components/forms/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import Field from "../Field";
 import FieldSet from "../FieldSet";
 
@@ -8,12 +8,17 @@ const RegistrationForm = () => {
             register,
             handleSubmit,
             formState: { errors },
+            control,
           } = useForm();
+
+          const {fields, append, remove} = useFieldArray({
+            name: 'socials',
+            control,
+          });
         
+          // fromData
           const submitForm = (data) => {
-            console.log(data);
-            
-            
+            console.log(data);     
           };
 
       return (
@@ -80,6 +85,53 @@ const RegistrationForm = () => {
               placeholder="Enter your Password"
             />
           </Field>
+        </FieldSet>
+
+        <FieldSet label='Enter Social Handles'>
+              {
+                fields.map((field, index)=>{
+                  return (
+                    <div className="flex justify-between items-center w-max" key={index}>
+                    <Field label="Socials" error={errors.socials}>
+                    <input
+                      {...register(`socials[${index}].name`, { required: "Socials is required" })}
+                      className={`p-2 border box-border  w-[300px] rounded-md ${
+                        errors.name ? "border-red-600" : "border-gray-400"
+                      }`}
+                      type="text"
+                      name={`socials[${index}].name`}
+                      id={`socials[${index}]`}
+                      placeholder="Enter your socials"
+                    />
+                   </Field>
+                    <Field label="SocialURL" error={errors.socials}>
+                    <input
+                      {...register(`socials[${index}].url`, { required: "Socials is required" })}
+                      className={`p-2 border box-border  w-[300px] rounded-md ${
+                        errors.name ? "border-red-600" : "border-gray-400"
+                      }`}
+                      type="text"
+                      name={`socials[${index}].url`}
+                      id={`socials[${index}]`}
+                      placeholder="Enter your socialURL"
+                    />
+                   </Field>
+                   <button
+                   className="mt-8 mr-2 text-red-500 font-bold"
+                   onClick={()=>remove(index)}>
+                     Remove
+                   </button>
+                    </div>
+                  )
+                })
+              }
+
+              <button
+              className="mt-8 text-md text-white cursor-pointer border rounded-lg bg-gray-500 p-1 m-auto"
+              onClick={()=>append({name:"", url: ""})}
+              >
+              Add a Social Handle
+              </button>
         </FieldSet>
           <div>{errors?.root?.random?.message}</div>
         <Field>
